30dd88a4ba5f605c70f54698dd8b148c482b94c7
Oliversinn <https://Oliversinn@github.com>
2020-04-22 16:08:28 -0600

    agregue fasta y lector de fasta

--- digestion.py
+++ digestion.py
@@ -1,7 +1,13 @@
 import argparse
+from Bio import SeqIO
+
 
 help_text = "Protease used in experiment. Options: Tripsina, Elastasa, Proteinasa K, ArgC, AspN, Quimiotripsina, GluC, LysC, LysN"
 par=argparse.ArgumentParser(description='This script simulates a protein digestion')
 par.add_argument('--fasta','-f',dest='fasta',type=str,help='Regular expression for choosing files with the protein sequence',required=True)
 par.add_argument('--protease','-p',dest='protease',type=str,help=help_text,required=True)
 args=par.parse_args()
+
+
+records = list(SeqIO.parse(args.fasta, "fasta"))
+print(records[0].seq)
