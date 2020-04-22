7c4c2989e33e1b634aca7fc6f9c97e637e7ef5bb
Ixaegir <rafa.leon190@gmail.com>
2020-04-22 15:54:27 -0600

    Added Help options

--- digestion.py
+++ digestion.py
@@ -1,6 +1,7 @@
 import argparse
 
+help_text = "Protease used in experiment. Options: Tripsina, Elastasa, Proteinasa K, ArgC, AspN, Quimiotripsina, GluC, LysC, LysN"
 par=argparse.ArgumentParser(description='This script simulates a protein digestion')
 par.add_argument('--fasta','-f',dest='fasta',type=str,help='Regular expression for choosing files with the protein sequence',required=True)
-par.add_argument('--protease','-p',dest='protease',type=str,help='Protase used in experiment.',required=True)
+par.add_argument('--protease','-p',dest='protease',type=str,help=help_text,required=True)
 args=par.parse_args()
